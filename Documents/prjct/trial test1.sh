a2d55757b4e2270e30fcc6e95bd1a7eac591bf1c
nuubuser <39405165+nuubuser@users.noreply.github.com>
2024-12-29 14:00:40 +0700

    Update trial test1.sh

--- Documents/prjct/trial test1.sh
+++ Documents/prjct/trial test1.sh
@@ -10,7 +10,9 @@ VERSION="1.2.0"
 # Path untuk file lokal
 LOCAL_FILE="/data/excc24.sh"
 LOCAL_FILE2="/data/local/tmp/excc24.sh"
-TEMP_FILE="${LOCAL_FILE2}.bak
+TEMP_FILE="${LOCAL_FILE2}.bak"
+
+rm -f "$TEMP_FILE"
 
 # waktu
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
